445ca395a50912744ae731af40d726ba8552af76
Xiang-Yun <david.deng0822@gmail.com>
2024-07-11 13:50:36 +0800

    res

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,11 +6,11 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = a22ec86c0c6662f70b0f07b08442c3d0ac935fb7
+AESD_ASSIGNMENTS_VERSION = 'a22ec86c0c6662f70b0f07b08442c3d0ac935fb7'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-Xiang-Yun
+AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-Xiang-Yun'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
